cbf542de411b68f12ad5404d3702bd367c971193
נתנאל ופלורה <netanelhemi90@gmail.com>
2023-03-05 20:01:52 +0200

    חדש

---
+++ src/tests/testSkill.java
@@ -0,0 +1,21 @@
+package tests;
+
+public class testSkill {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+        print();
+        System.out.println("my name is Netanel");
+        print();
+//היי
+
+    }
+    public static void print() {
+        for (int i = 0; i < 15 ;i++){
+            System.out.print("_");
+        }
+        System.out.println();
+    }
+}
+
